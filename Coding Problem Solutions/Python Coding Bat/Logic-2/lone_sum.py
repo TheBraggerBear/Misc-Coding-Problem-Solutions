428455bd9b2688a51baa1048f0a194d00bd8d73f
Aiden Burgstahler <158793389+TheBraggerBear@users.noreply.github.com>
2026-01-12 00:04:01 -0600

    Add solutions for Python Coding Bat List-1 and Logic-2
    Added Python implementations for several List-1 problems (common_end, first_last6, has23, make_ends, make_pi, max_end3, middle_way, reverse3, rotate_left3, same_first_last, sum2, sum3) and some Logic-2 problems (lone_sum, make_bricks). Added problem stubs for close_far, lucky_sum, make_chocolate, no_teen_sum, and round_sum in Logic-2.

---
+++ Coding Problem Solutions/Python Coding Bat/Logic-2/lone_sum.py
@@ -0,0 +1,11 @@
+'''
+Given 3 int values, a b c, return their sum. 
+However, if one of the values is the same as another of the values, it does not count towards the sum.
+
+lone_sum(1, 2, 3) → 6
+lone_sum(3, 2, 3) → 2
+lone_sum(3, 3, 3) → 0
+'''
+def lone_sum(a, b, c):
+  arr = [a,b,c]
+  return sum([x for x in set(arr) if arr.count(x) <= 1])
