1c739dee7d74bac0edf7231f5abf7f5c2ebcd53e
Aiden Burgstahler <158793389+TheBraggerBear@users.noreply.github.com>
2026-03-08 01:17:04 -0600

    Started java codingbat

---
+++ Coding Problem Solutions/Coding Bat/Java Coding Bat/Warmup-1/Front22.java
@@ -0,0 +1,14 @@
+/*
+Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". 
+If the string length is less than 2, use whatever chars are there.
+
+front22("kitten") → "kikittenki"
+front22("Ha") → "HaHaHa"
+front22("abc") → "ababcab"
+*/
+
+public class Front22 {
+    public String front22(String str) {
+        return str.length() < 2 ? String.join("", str,str,str) : str.substring(0,2) + str + str.substring(0,2);
+    }
+}
---
+++ Coding Problem Solutions/Coding Bat/Java Coding Bat/Warmup-1/FrontBack.java
@@ -0,0 +1,16 @@
+/*
+Given a string, return a new string where the first and last chars have been exchanged.
+
+frontBack("code") → "eodc"
+frontBack("a") → "a"
+frontBack("ab") → "ba"
+*/
+
+public class FrontBack {
+    public String frontBack(String str) {
+        if (str.length() > 1) {
+            return str.substring(str.length() - 1) + str.substring(1, str.length() - 1) + str.substring(0, 1);
+        }
+        return str;
+    }
+}
